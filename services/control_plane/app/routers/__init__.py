a14f8ebabf8a9d57d5b119ab07495b8905680025
Godson Ajodo <godsonajodo2020@gmail.com>
2025-10-17 00:49:50 -0700

    important routers

--- services/control_plane/app/routers/__init__.py
+++ services/control_plane/app/routers/__init__.py
@@ -1,6 +1,12 @@
 """Routers module for control plane."""
 from .health import router as health_router
 from .videos import router as videos_router
-from .admin import router as admin_router
+from .admin import customers_router, api_keys_router, credits_router
 
-__all__ = ["health_router", "videos_router", "admin_router"]
+__all__ = [
+    "health_router",
+    "videos_router", 
+    "customers_router",
+    "api_keys_router",
+    "credits_router"
+]
